ec156aeb921f59b5be9f0dd11d5d84709d00334a
Shreyas <tupkarshreyas@gmail.com>
2024-12-16 14:10:05 +0530

    Data Ingestion

--- setup.py
+++ setup.py
@@ -1,4 +1,22 @@
 from setuptools import find_packages,setup
+from typing import List
+
+HYPHEN_E_DOT='e .'
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will return the list of requirements
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        [req.replace("\n","") for req in requirements]
+
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+
+    return requirements
+
+
 
 setup(
     name='mlproject',
@@ -6,5 +24,5 @@ setup(
     author='shreyas',
     author_email='tupkarshreyas@gmail.com',
     packages=find_packages(),
-    install_requires=['pandas','numpy','seaborn']
+    install_requires=get_requirements('requirements.txt')
 )
